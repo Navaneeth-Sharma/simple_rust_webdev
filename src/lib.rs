f20f9b7ba7d7bf55d45f2d7e856f951e49b4f0be
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2023-10-22 23:56:08 +0530

    add mod.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,11 @@
 mod routes;
-use axum::{routing::get, Router};
 
 pub async fn run() {
-    let app = Router::new().route("/", get(hello_world));
+    //let app = Router::new().route("/", get(hello_world));
 
+    let app = routes::create_routes();
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
-}
-
-async fn hello_world() -> String {
-    "Hello! World. Cool man".to_owned()
 }
