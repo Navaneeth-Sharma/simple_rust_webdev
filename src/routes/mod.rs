a2d95036f4066f360dda14cc88c4acfd097fb8e0
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2023-10-23 16:08:52 +0530

    add a simple string -> string post req

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,5 +1,12 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
+mod body_string;
+use body_string::body_string;
 
 pub fn create_routes() -> Router {
-    Router::new().route("/", get(|| async { "Hello World!" }))
+    Router::new()
+        .route("/", get(|| async { "Hello World!" }))
+        .route("/mirror_body_string", post(body_string))
 }
