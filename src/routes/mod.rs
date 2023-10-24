945160a0d81db560c2ed7cf2d3c2add90d05aa60
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2023-10-24 23:01:22 +0530

    add middleware stuffs

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,4 +1,5 @@
 use axum::{
+    http::Method,
     routing::{get, post},
     Router,
 };
@@ -14,9 +15,14 @@ use body_string::body_string;
 use custom_header::custom_header;
 use path_variables::path_variables;
 use query_params::query_params;
+use tower_http::cors::{Any, CorsLayer};
 use user_agent::user_agent;
 
 pub fn create_routes() -> Router {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any);
+
     Router::new()
         .route("/", get(|| async { "Hello World!" }))
         .route("/mirror_body_string", post(body_string))
@@ -25,4 +31,5 @@ pub fn create_routes() -> Router {
         .route("/query_params", get(query_params))
         .route("/mirror_user_agent", get(user_agent))
         .route("/mirror_custom_header", get(custom_header))
+        .layer(cors)
 }
