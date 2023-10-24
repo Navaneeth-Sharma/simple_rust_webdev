3f07d89f43904b98b195fc72a22a64957c42a4dc
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2023-10-24 12:54:30 +0530

    add json

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,11 +2,18 @@ use axum::{
     routing::{get, post},
     Router,
 };
+mod body_json;
 mod body_string;
+mod path_variables;
+
+use body_json::body_json;
 use body_string::body_string;
+use path_variables::path_variables;
 
 pub fn create_routes() -> Router {
     Router::new()
         .route("/", get(|| async { "Hello World!" }))
         .route("/mirror_body_string", post(body_string))
+        .route("/mirror_body_json", post(body_json))
+        .route("/path_variables/:id", get(path_variables))
 }
